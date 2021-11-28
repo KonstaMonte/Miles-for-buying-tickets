baa6bc71536eb70bc9cc1a944a83b291632deec1
Alexeev Konstantin <latituderus199@gmail.com>
2021-11-28 14:04:51 +0100

    miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        int costMile = 20;
+        int numberMiles = ticketPrice / costMile;
+        System.out.println(numberMiles);
+    }
+
+}
+
